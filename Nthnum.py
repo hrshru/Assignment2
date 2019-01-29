b34021107e88282f3a8509f269dc31839566d40d
Hari Suryawanshi <43181943+hrshru@users.noreply.github.com>
2019-01-30 02:05:25 +0530

    Update Nthnum.py

--- Nthnum.py
+++ Nthnum.py
@@ -1,3 +1,4 @@
+#Question1- Program to find the sum of first N numbers which are either multiple of 3 or 5
 n=int(input("Enter the Nth Number:"))
 for i in range(1,n+1):
     if(i%3==0) or (i%5==0):
